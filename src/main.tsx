4182d1d1ac69d2972455cd1f2286edba5163b52b
Keev003 <keev003@student.kristiania.no>
2025-02-27 18:24:45 +0100

    Updated with prettier

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,23 @@ import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "ol/ol.css";
 
 useGeographic();
 
 const map = new Map({
-    view: new View({ center: [10.8, 59.9], zoom: 13 }),
-    layers: [new TileLayer({ source: new OSM() })],
+  view: new View({ center: [10.8, 59.9], zoom: 13 }),
+  layers: [new TileLayer({ source: new OSM() })],
 });
 
 // A functional React component
 export function Application() {
-    const mapRef = useRef<HTMLDivElement | null>(null);
-    useEffect(() => {
-        map.setTarget(mapRef.current!);
-    }, []);
-    return <div ref={mapRef}></div>;
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    map.setTarget(mapRef.current!);
+  }, []);
+  return <div ref={mapRef}></div>;
 }
 
-createRoot(document.getElementById("root")).render(<Application/>)
+createRoot(document.getElementById("root")).render(<Application />);
