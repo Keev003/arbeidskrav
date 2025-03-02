babd38c7ebdba144e5b38456b41d913b920b5af3
Keev003 <keev003@student.kristiania.no>
2025-03-02 21:02:38 +0100

    Initial hono server

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,8 @@
-console.log("Hello");
+import {Hono} from "hono";
+import {serve} from "@hono/node-server";
+
+const app = new Hono();
+app.get("/", async (c) => {
+    return c.text("Hello SOMEBODY");
+});
+serve(app);
