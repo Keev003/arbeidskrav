6beab484cd3a9fe086946a332e5a6c4dd81c99b9
Keev003 <keev003@student.kristiania.no>
2025-03-02 13:05:00 +0100

    Updated shelters on map

---
+++ src/app/application.tsx
@@ -0,0 +1,44 @@
+import {Map, View} from "ol";
+import TileLayer from "ol/layer/Tile";
+import {OSM} from "ol/source";
+import React, {useEffect, useRef, useState} from "react";
+import {useGeographic} from "ol/proj";
+import VectorSource from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
+import {Layer} from "ol/layer";
+import {ZoomToMeButton} from "../location/zoomToMeButton";
+import {ShowMeCheckbox} from "../location/showMeCheckbox";
+import {GeoJSON} from "ol/format";
+
+useGeographic();
+
+const userSource = new VectorSource();
+const view = new View({ center: [10.8, 59.9], zoom: 10 });
+
+function MapView() {
+    const mapRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        map.setTarget(mapRef.current!);
+        return () => map.setTarget(undefined);
+    }, []);
+    return <div ref={mapRef}></div>;
+}
+
+const osmLayer = new TileLayer({ source: new OSM() });
+const civilDefenseLayer = new VectorLayer({
+    source: new VectorSource({
+        url: "/geojson/tilfluktsrom.geojson",
+        format: new GeoJSON(),
+    }),
+});
+
+const map = new Map({
+    view: new View({ center: [10.8, 59.9], zoom: 8 }),
+    layers: [osmLayer, civilDefenseLayer],
+});
+
+export function Application() {
+    const mapRef = useRef<HTMLDivElement | null>(null);
+    useEffect(() => map.setTarget(mapRef.current!), []);
+    return <div ref={mapRef}></div>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,10 @@
 import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
-import TileLayer from "ol/layer/Tile";
-import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 import { createRoot } from "react-dom/client";
 import "ol/ol.css";
+import {Application} from "./app/application";
 
 useGeographic();
 
-const map = new Map({
-  view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  layers: [new TileLayer({ source: new OSM() })],
-});
-
-// A functional React component
-export function Application() {
-  const mapRef = useRef<HTMLDivElement | null>(null);
-  useEffect(() => {
-    map.setTarget(mapRef.current!);
-  }, []);
-  return <div ref={mapRef}></div>;
-}
-
 createRoot(document.getElementById("root")).render(<Application />);
+
